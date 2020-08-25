1516b57cc5c60eb1802540942d4a232b1d4732d8
HigorReculiano <you@example.com>
2020-08-25 19:17:35 -0300

    versão 2.0

--- src/javaapplication1/Aluno1.java
+++ src/javaapplication1/Aluno1.java
@@ -11,25 +11,30 @@ public class Aluno1 {
     
     private String nome1;
     private String nome2;
+    private String nome3;
     
     public void salvarNome(){
         String nomeAux1;
         String nomeAux2;
+        String nomeAux3;
         Scanner scan = new Scanner(System.in);
         System.out.print("Digite um nome:");
         nomeAux1 = scan.nextLine();
         System.out.print("Digite outro nome:");
         nomeAux2 = scan.nextLine();
+        System.out.print("Digite o ultimo nome:");
+        nomeAux3 = scan.nextLine();
         
-        aplicarTexto(nomeAux1, nomeAux2);
+        aplicarTexto(nomeAux1, nomeAux2, nomeAux3);
     }
     
-    public void aplicarTexto(String nome1, String nome2){
+    public void aplicarTexto(String nome1, String nome2, String nome3){
         this.nome1 = nome1;
         this.nome2 = nome2;
+        this.nome3 = nome3;
     }
     
     public void mostrarNomes(){
-        System.out.println("Pessoa 1: " + nome1 + "/// Pessoa 2: " + nome2);
+        System.out.println("Pessoa 1: " + nome1 + "/// Pessoa 2: " + nome2 + "/// Pessoa 3:" + nome3);
     }
 }
